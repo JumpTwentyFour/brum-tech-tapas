be081ffe43ff1e29e3141986ac6843fac4719176
Anneka Mistry <anneka@jump24.co.uk>
2020-09-22 11:30:47 +0100

    UPDATE: triggering svg animation

--- src/js/logo-animation.js
+++ src/js/logo-animation.js
@@ -5,18 +5,32 @@ const path = require('path');
 export default class LogoAnimation {
 
   animObj = null;
+  animContainer = document.getElementById('about__image');
 
   init = () => {
-    this.loadAnimation();
+    if (this.animContainer.length !== null) {
+      this.setupListener();
+    }
   };
 
   loadAnimation = () => {
     this.animObj = lottie.loadAnimation({
-      container: document.getElementById('about__image'),
+      container: this.animContainer,
       renderer: 'svg',
       loop: false,
       autoplay: true,
       path: path.resolve(__dirname, 'dist/logo.json')
     });
+  };
+
+  setupListener = () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0.2) {
+          this.loadAnimation();
+        }
+      });
+    });
+    observer.observe(this.animContainer);
   };
 }
