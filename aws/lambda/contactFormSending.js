8bf8444a14fbce8e44593895dfc25683b24ce04d
Daniel newns <dan@jump24.co.uk>
2021-03-07 10:49:00 +0000

    update: adding in initial lambda method

---
+++ aws/lambda/contactFormSending.js
@@ -0,0 +1,47 @@
+const AWS = require('aws-sdk');
+
+const ses = new AWS.SES();
+
+const RECEIVER = 'brumtechtapas@jump24.co.uk';
+const SENDER = 'brumtechtapas@jump24.co.uk';
+
+const response = {
+  isBase64Encoded: false,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'example.com',
+  },
+  statusCode: 200,
+  body: '{"result": "Success."}',
+};
+
+exports.handler = function (event, context) {
+  console.log('Received event:', event);
+  sendEmail(event, (err, data) => {
+    context.done(err, null);
+  });
+};
+
+function sendEmail(event, done) {
+  const params = {
+    Destination: {
+      ToAddresses: [
+        RECEIVER,
+      ],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: `name: ${event.name}\nphone: ${event.phone}\nemail: ${event.email}\ndesc: ${event.desc}`,
+          Charset: 'UTF-8',
+        },
+      },
+      Subject: {
+        Data: `Website Referral Form: ${event.name}`,
+        Charset: 'UTF-8',
+      },
+    },
+    Source: SENDER,
+  };
+  ses.sendEmail(params, done);
+}
