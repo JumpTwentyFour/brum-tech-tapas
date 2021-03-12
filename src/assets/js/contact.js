e51605a9fcaadf3a0cc53ba87f0b100a65c73efb
Anneka Mistry <anneka@jump24.co.uk>
2021-03-12 13:33:03 +0000

    UPDATE: form thank you message

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -9,9 +9,9 @@ const TellUsMore = class TellUsMore {
 
   signUpTrigger = document.querySelectorAll('.js-form-btn')
 
-  formTitle = document.getElementById('js-form-title')
+  formHeader = document.getElementById('js-form-header')
 
-  formContainer = document.getElementById('js-contact-form')
+  formTitle = document.getElementById('js-form-title')
 
   formFields = this.formContainer.querySelectorAll('.field')
 
@@ -98,6 +98,9 @@ const TellUsMore = class TellUsMore {
 
   showThanksMessage = () => {
     this.formTitle.innerText = 'Thank you';
+
+    this.formHeader.classList.remove('col-span-7', 'col-start-3', 'm-col-start-2', 'm-col-span-9');
+    this.formHeader.classList.add('col-span-5', 'col-start-5', 'm-col-start-5', 'm-col-span-5');
 
     this.formFields.forEach( (formFields) => {
       formFields.style.opacity = '0';
